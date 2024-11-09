b3d49fd8f8c45ab0fda55110fb2df2ab6d99dab4
ramadani <131200011+ramadanramad@users.noreply.github.com>
2024-11-09 08:20:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeInElements = document.querySelectorAll('.home-text h1, .home-text .lorem-text, .home-image, .read-button');
-
-    fadeInElements.forEach(element => {
-        element.classList.add('visible');
-    });
-});
